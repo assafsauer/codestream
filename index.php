84b1bee81d9556e3dc7ee2106125e996f53be942
assafsauer <as.sauer@gmail.com>
2019-08-16 20:41:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
+<?php
+$var = 5;
+?>
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <p>Hello World</p>
- </body>
+<head>
+<title>My Page</title>
+</head>
+<body>
+<h2>Your variable was <?php echo $var; ?></h2>
+</body>
 </html>
